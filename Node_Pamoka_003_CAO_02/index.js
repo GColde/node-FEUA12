08afdeee65acef7c9e1be095c94070b47d9d9ebb
GColde <gantasgan@gmail.com>
2024-01-25 20:26:33 +0100

    pamoka_003

---
+++ Node_Pamoka_003_CAO_02/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+const brands = [{ name: "Bake", surname: "Cake" }];
+
+app.get("/brands", (req, res) => {
+  res.send(brands);
+});
+
+app.get("/brands/:firstLetter", (req, res) => {
+  const { firstLetter } = req.params;
+  console.log(firstLetter);
+
+  const filteredBrands = brands.filter(
+    (brand) => brand[0].toLowerCase() === firstLetter.toLowerCase()
+  );
+
+  res.send(filteredBrands);
+});
+
+app.post("/brands", (req, res) => {
+  const { name, surname } = req.body;
+  brands.push({ name, surname });
+  res.send(brands);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
